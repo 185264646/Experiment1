fdef0dac2aefafdd6aa370d4e2a381821eba42d8
185264646 <43387518+185264646@users.noreply.github.com>
2020-10-19 01:23:09 +0800

    An enhanced version of 1.4.c

---
+++ 1.4-new.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include<string.h>
+void encrypt(char *c);
+inline void encrypt(char *c)
+{
+	_Bool bit = *c & 0x40;
+	*c = ((*c >> 1 & 0x15) | (*c << 1 & 0x3A)) & 0x3F;
+	if (bit)
+	{
+		bit = *c & 0x20;
+		*c = (*c << 1) | 0x40 | bit;
+	}
+	return;
+}
+
+int main(void)
+{
+	char a[BUFSIZ];
+	int i, len;
+	fgets(a, BUFSIZ - 1, stdin);
+	a[strlen(a) - 1] = '\0';
+	len = strlen(a);
+	for (i = 0; i < len; i++)
+		encrypt(a + i);
+	puts(a);
+	return 0;
+}
